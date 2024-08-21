dfcaac8fa4a6e47b32351751bbdd4abe8cc5b491
FastikProg <126092637+fastikprog@users.noreply.github.com>
2024-08-21 15:46:50 +0400

    Мини изменения в конфигурация
    Теперь удобнее стало добавлять новые значения для генерация Конфигурационного файла и дефаулт значения к ним

--- data/config.py
+++ data/config.py
@@ -27,7 +27,7 @@ class SecretConfig:
         """
         if not self.env_file or not os.path.exists(self.env_file):
             self.generate_env_file()
-            self.load_env()  
+            self.load_env()
         else:
             load_dotenv(self.env_file)
 
@@ -35,12 +35,11 @@ class SecretConfig:
         self,
         key: str,
         default: Optional[Union[str, int]] = None
-    ) -> Optional[str]:
+    ) -> Optional[Union[str, int]]:
         """
         Получить значение секрета по ключу.
 
-        Сначала пытается получить из переменных окружения,
-        затем из .env файла.
+        Если ключ не найден, добавляет его в .env файл с дефолтным значением.
 
         Args:
             key (str): Ключ для доступа к секрету.
@@ -49,21 +48,77 @@ class SecretConfig:
         Returns:
             str: Значение секрета, если найдено, иначе значение по умолчанию.
         """
-        return os.getenv(key, default)
+        value = os.getenv(
+            key=key
+        )
+
+        if value is None:
+            value = default
+            self.set_secret(key, value)
+
+        return value
+
+    def set_secret(
+        self,
+        key: str,
+        value: Union[str, int]
+    ) -> None:
+        """
+        Добавить ключ и значение в .env файл, если они отсутствуют.
+
+        Args:
+            key (str): Ключ для секрета.
+            value (str): Значение секрета.
+        """
+        with open(self.env_file, 'a', encoding='utf-8') as file:
+            file.write(f'\n{key}={value}')
+        load_dotenv(self.env_file, override=True)
 
     @staticmethod
     def generate_env_file():
         print("Файл .env не найден. Пожалуйста, введите необходимые значения:")
-        bot_api_token = input("Токен телеграм бота от @botfather: ")
-        if_always_private = input("Всегда обрабатывать только в личных сообщениях (1 для включения, 0 для отключения): ")
-        anti_flood_interval = input("Анти-флуд система (рекомендуемое значение 0.7): ")
 
+        # Словарь с категориями и их соответствующими настройками
+        config_structure = {
+            "Токены и ключи": {
+                "BOT_API_TOKEN": "Токен телеграм бота от @botfather: "
+            },
+            "Основные настройки": {
+                "If_ALWAYS_PRIVATE": "Всегда обрабатывать только в личных сообщениях (1 для включения, 0 для отключения): ",
+                "ANTI_FLOOD_INTERVAL": "Анти-флуд система (рекомендуемое значение 0.7): "
+            },
+            "Настройки базы данных": {
+                "DATABASE_LOGIN": "Логин для базы данных (по умолчанию root): ",
+                "DATABASE_PASSWORD": "Пароль для базы данных: ",
+                "DATABASE_NAME": "Название базы данных (en): ",
+                "DATABASE_IP": "IP адрес базы данных (по умолчанию localhost): ",
+                "DATABASE_PORT": "Порт базы данных (по умолчанию 3306): "
+            }
+        }
+
+        config_defaults = {
+            "DATABASE_LOGIN": 'root',
+            "If_ALWAYS_PRIVATE": 0,
+            "ANTI_FLOOD_INTERVAL": 0.7,
+            "DATABASE_IP": "localhost",
+            "DATABASE_PORT": 3306
+        }
+
+        config_values = {}
+
+        # Запрос значений у пользователя
+        for category, prompts in config_structure.items():
+            print(f"\n{category}:")
+            for key, prompt in prompts.items():
+                value = input(prompt) or str(config_defaults.get(key, ""))
+                config_values[key] = value
+
+        # Запись в .env файл с разделением по категориям
         with open('.env', 'w', encoding='utf-8') as file:
-            file.write(f'''# ! Токен телеграм бота от @botfather
-BOT_API_TOKEN={bot_api_token}
+            for category, prompts in config_structure.items():
+                file.write(f"\n# {category}\n")
+                for key in prompts.keys():
+                    value = config_values[key]
+                    file.write(f"{key}={value}\n")
 
-# ? Всегда обрабатывать только в личных сообщениях. Если везде только в личных - 1, если хотите отключить то 0. По умол. 0:
-If_ALWAYS_PRIVATE={if_always_private}
-
-# ? Анти флуд система: Возможные типы float, integer (1 | 1.11). При значения 0 отключается. НЕ РЕКАМЕНДУЕТСЯ. Рекамендуемое значения 0.7
-ANTI_FLOOD_INTERVAL={anti_flood_interval}''')
+        print("Конфигурационный файл успешно создан.")
